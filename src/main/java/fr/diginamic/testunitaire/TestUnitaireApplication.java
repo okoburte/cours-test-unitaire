f035ea5be5535ace5e6f997e9eb5cf6fd7e044d9
okoburte <jburte34@gmail.com>
2025-10-02 10:41:19 +0200

    first commit

---
+++ src/main/java/fr/diginamic/testunitaire/TestUnitaireApplication.java
@@ -0,0 +1,13 @@
+package fr.diginamic.testunitaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestUnitaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestUnitaireApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/diginamic/testunitaire/TestUnitaireApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.diginamic.testunitaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestUnitaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
